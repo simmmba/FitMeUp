29439613ef45f5cce4beaaf346f29da67ae6ea8b
송다은 <ssongda9412@naver.com>
2020-05-18 22:34:43 +0900

    0518 | message send | DE, S02P31A201-25

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import "./Main.scss";
 
+import withFirebaseAuth from "react-with-firebase-auth";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "../../firebaseConfig";
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       login: false,
+      code:""
     };
   }
 
@@ -16,16 +28,85 @@ class Home extends React.Component {
         login: true,
       });
     }
+
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier("recaptcha-container",
+    {
+       size:"invisible"
+        // other options
+    });
   }
 
+  onClick() {
+    const phoneNumber = "+8201079396800";
+    const appVerifier = window.recaptchaVerifier;
+    firebase
+    .auth()
+    .signInWithPhoneNumber(phoneNumber, appVerifier)
+    .then(confirmResult => {
+      // success
+      window.confirmationResult  = confirmResult;
+      console.log(confirmResult)
+    })
+    .catch(error => {
+      // error
+      console.log(error)
+    });
+  }
+
+  onCheck = () => {
+    window.confirmationResult.confirm(this.state.code).then(function (result) {
+      // User signed in successfully.
+      // var user = result.user;
+      console.log(result)
+      // ...
+    }).catch(function (error) {
+      // User couldn't sign in (bad verification code?)
+      // ...
+    });
+  }
+
+  onChange = (e) => {
+    this.setState({
+      code:e.target.value
+    })
+  }
+
+
   render() {
+    const { user, signOut, signInWithGoogle } = this.props;
+    console.log(this.props.user && user.providerData[0]);
     return (
       <div className="Main">
         <div>로그인하기</div>
         <div>회원가입하기</div>
+        {user ? (
+          <button onClick={signOut}>Sign out</button>
+        ) : (
+          <input
+            id="recaptcha-container"
+            type="button"
+            onClick={this.onClick}
+            value="클릭"
+          />
+          
+          // <button onClick={signInWithGoogle}>Sign in with Google</button>
+        )}
+        <input value={this.state.code} onChange={this.onChange}></input>
+        <input
+            type="button"
+            onClick={this.onCheck}
+            value="확인"
+          />
       </div>
     );
   }
 }
 
-export default Home;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(Home);
+
+//https://medium.com/firebase-developers/how-to-setup-firebase-authentication-with-react-in-5-minutes-maybe-10-bb8bb53e8834
+//https://firebase.google.com/docs/auth/web/phone-auth
+//https://stackoverflow.com/questions/51512893/firebase-phone-auth-react-js
