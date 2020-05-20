7c2f12c2f8cd8b1c33ab85ea16b1999be979d5a8
silloun <47407845+silloun@users.noreply.github.com>
2020-05-20 11:17:27 +0900

    0420 | update userAPI | JH, S02P31A201-51

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,59 +1,59 @@
 import { user } from "../models"
-
+import {Op} from "Sequelize"
 // 유저 생성
 export const create_user = async function (req, res, ) {
 
     try {
         // body로 부터 param 추출
         const {
-            type, gender, age, nickname, profile_img, platform, api_id, name, phone,
+            type, gender, age, nickname, profile_img, platform, api_id, name, belong, occupation, phone,
         } = req.body;
 
         // 가입된 회원 여부 확인
         const user_exist = await user.findOne({ where: { api_id: api_id } })
         
         if (user_exist) {
-            // res.json( {state: "Fail", detail: "user exist"});
+            // res.json( {result: "Fail", detail: "user exist"});
             throw new Error("user exist");
         }
         else {
             const new_user = await user.create({
-                type, gender, age, nickname, profile_img, platform, api_id, name, phone
+                type, gender, age, nickname, profile_img, platform, api_id, name, belong, occupation,phone
             });
 
             if (new_user) {
-                res.json({ state: "Success", user: new_user });
+                res.json({ result: "Success", user: new_user });
             } else {
-                res.json({ state: "Fail", detail: "not create" });
+                res.json({ result: "Fail", detail: "not create" });
             }
         }
     } catch (err) {
         console.log(err);
-        res.status(500).json({ state: "Fail", detail: "500 Internal Server Error" });
+        res.status(500).json({ result: "Fail", detail: "500 Internal Server Error" });
     }
 }
 
 export const read_user = async (req, res) => {
     try {
 
-        const { api_id, platform } = req.query
-        const user_find = await user.findOne({ where: { api_id, platform } })
+        const { user_id } = req.query
+        const user_find = await user.findOne({ where: { id:user_id } })
         // delete user.dataValues.password
         // delete user.dataValues.auth
         if (user_find) {
-            res.json({ state: "Success", user: user_find });
+            res.json({ result: "Success", user: user_find });
         } else {
-            res.json({ state: "Fail", detail: "user not found" });
+            res.json({ result: "Fail", detail: "user not found" });
         }
     } catch (err) {
         console.log(err);
-        res.status(500).json({ state: "Fail", detail: "500 Internal Server Error" });
+        res.status(500).json({ result: "Fail", detail: "500 Internal Server Error" });
     }
 }
 
 export const update_user = async (req, res) => {
     try {
-        const { type, gender, age, nickname, profile_img, platform, api_id, name, phone } = req.body.user;
+        const { type, gender, age, nickname, profile_img, platform, api_id, name, phone } = req.body;
         
         console.log(req.body);
         
@@ -61,11 +61,11 @@ export const update_user = async (req, res) => {
             where: { api_id}
         })
         
-        res.json({state :" Success", user: req.body.user})
+        res.json({result :" Success", user: req.body.user})
 
     } catch (err) {
         console.log(err);
-        res.status(500).json({ state: "Fail", detail: "500 Internal Server Error" });
+        res.status(500).json({ result: "Fail", detail: "500 Internal Server Error" });
     }
 }
 
@@ -73,18 +73,87 @@ export const delete_user = async (req, res) => {
     const {api_id} = req.body
     try {
         let user_delete = await user.destroy({ where: {api_id} })
-        res.json({state:"Success"});
+        res.json({result:"Success"});
     } catch (err) {
         console.log(err);
-        res.status(500).json({ state: "Fail", detail: "500 Internal Server Error" });
+        res.status(500).json({ result: "Fail", detail: "500 Internal Server Error" });
     }
 }
 
+export const login = async (req, res) => {
+    try {
+        
+        const { api_id, platform } = req.query
+        let user_find = await user.findOne({ where: { api_id, platform } })
+        // delete user.dataValues.password
+        // delete user.dataValues.auth
+        if (user_find) {
+            res.json({ result: "Success", user: user_find });
+        } else {
+            res.json({ result: "Fail", detail: "user not found" });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ result: "Fail", detail: "500 Internal Server Error" });
+    }
+}
 
+export const duplicate = async (req,res) => {
+    
+    try {
+        const {nickname} = req.query
+        let is_exist = await user.findOne({where : {nickname}})
+        console.log(is_exist);
+        
+        if(is_exist){
+            res.json({result : "Success", isDuplicate : false})
+        }else{
+            res.json({result : "Success", isDuplicate : true})
+        }
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ result: "Fail", detail: "500 Internal Server Error" });
+    }
+    
+} 
 
+export const stylist_list = async (req, res) => {
+    try {
+        let list = await user.findAll({where:{type:"stylist"}})
+        
+        res.json({result : "Success", stylists : list})
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ result: "Fail", detail: "500 Internal Server Error" });
+    }
+}
 
-
-
+export const search = async (req, res) => {
+    try {
+        const {option, keyword} = req.query;
+        let stylist_search;
+        let word = '%'+keyword+'%'
+        if(option === "nickname"){
+            stylist_search = await user.findAll({where:{nickname:{[Op.like]:word}, type:"stylist"}})
+        }else{
+            stylist_search = await user.findAll({where:{
+                [Op.or]:[
+                    {nickname:{[Op.like]:word}},
+                    {name:{[Op.like]:word}},
+                    {belong:{[Op.like]:word}}
+                ]
+                ,type:"stylist"
+            }})
+        }
+        res.json({result:"Success", stylists:stylist_search})
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ result: "Fail", detail: "500 Internal Server Error" });
+    }
+}
 
 
 
