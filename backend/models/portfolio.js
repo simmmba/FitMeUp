b87c78740359918e52010cdfb3dd4f3b536aa18c
silloun <47407845+silloun@users.noreply.github.com>
2020-06-01 15:51:45 +0900

    0601 | modify upload | JH

--- backend/models/portfolio.js
+++ backend/models/portfolio.js
@@ -23,6 +23,10 @@ module.exports = function(sequelize, DataTypes) {
         contents: {
             type: DataTypes.STRING(2000),
             allowNull: true
+        },
+        main_img : {
+            type:DataTypes.STRING(100),
+            allowNull : true
         }
     }, {
         tableName: 'portfolio'
