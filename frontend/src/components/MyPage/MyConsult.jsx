e5b627fc87e2e65aca0c781df62db111ead7ea87
simmmba <jsy9532@gmail.com>
2020-06-07 22:07:43 +0900

    0607 | change mypage ui | SW

---
+++ frontend/src/components/MyPage/MyConsult.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import "./MyPageMain.scss";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const MyConsult = () => {
+  return <div className="middle_tab col-6">상담 내역</div>;
+};
+
+export default MyConsult;
