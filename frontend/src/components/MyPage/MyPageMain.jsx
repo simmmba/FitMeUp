8afb8fc41b9dc2fda91648c11b41337de1c81e00
simmmba <jsy9532@gmail.com>
2020-06-09 02:57:04 +0900

    0609 | fix mypage | SW

--- frontend/src/components/MyPage/MyPageMain.jsx
+++ frontend/src/components/MyPage/MyPageMain.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import {NavLink, Route, Switch} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { NavLink, Route, Switch } from "react-router-dom";
 import Header from "../Common/Header";
 import "./MyPageMain.scss";
 import GeneralThirdTab from "./GeneralThirdTab";
@@ -11,26 +11,12 @@ import MyFrequent from "./MyFrequent";
 import StylistMyInfo from "./StylistMyInfo";
 import StylistStats from "./StylistStats";
 import CreditHistory from "./CreditHistory";
-import axios from 'axios'
+import axios from "axios";
 
 const MyPageMain = () => {
   const loginUser = JSON.parse(window.sessionStorage.getItem("user"));
   const [generalUserFlag, setGeneralUserFlag] = useState(false);
   const [rightTab, setRightTab] = useState("freq");
-    const [isPortfolioExist, setIsPortfolioExist] = useState(true);
-
-    useEffect(() => {
-        if(loginUser.type==="stylist") {
-            axios.get(`${process.env.REACT_APP_URL}/portfolio/check?stylist_id=` + loginUser.id)
-                .then(res => {
-                    if(res.data.isExist==="true") {
-                        setIsPortfolioExist(true)
-                    } else {
-                        setIsPortfolioExist(false)
-                    }
-                })
-        }
-    }, [])
 
   const rightBtn = (e) => {
     setRightTab(e.target.id);
@@ -41,69 +27,60 @@ const MyPageMain = () => {
       <Header />
       <div className="MyPageBox">
         <div className="MyPageMain">
-            {loginUser.type==="general"?(
-                <div className="MyPageMenu">
-                    <div className="leftMenu"></div>
-                    <div className="middleMenu">
-                        <NavLink to="/mypage" className="selectBtn" activeClassName="selectedBtn" exact>
-                            내 상담
-                        </NavLink>
-                        <NavLink to="/mypage/myinfo" className="selectBtn" activeClassName="selectedBtn">
-                            내 정보
-                        </NavLink>
-                    </div>
-                    <div className="rightMenu">
-                        <button id="freq" className={rightTab === "freq" ? "selectedBtn" : "selectBtn"} onClick={rightBtn}>
-                            자주
-                        </button>
-                        <button id="msg" className={rightTab === "msg" ? "selectedBtn" : "selectBtn"} onClick={rightBtn}>
-                            메세지
-                        </button>
-                    </div>
+          {loginUser.type === "general" && (
+            <div className="MyPageMenu">
+              <div className="leftMenu"></div>
+              <div className="middleMenu">
+                <NavLink to="/mypage" className="selectBtn" activeClassName="selectedBtn" exact>
+                  내 상담
+                </NavLink>
+                <NavLink to="/mypage/myinfo" className="selectBtn" activeClassName="selectedBtn">
+                  내 정보
+                </NavLink>
+              </div>
+              <div className="rightMenu">
+                <button id="freq" className={rightTab === "freq" ? "selectedBtn" : "selectBtn"} onClick={rightBtn}>
+                  단골
+                </button>
+                <button id="msg" className={rightTab === "msg" ? "selectedBtn" : "selectBtn"} onClick={rightBtn}>
+                  메세지
+                </button>
+              </div>
+            </div>
+          )}
+          {loginUser.type === "general" ? (
+            <div className="contentBox">
+              <GeneralFirstTab />
+              <Switch>
+                <Route path={`/mypage`} component={MyConsult} exact={true} />
+                <Route path={`/mypage/myinfo`} component={MyInfo} />
+              </Switch>
+              {rightTab === "freq" ? <MyFrequent /> : <GeneralThirdTab />}
+            </div>
+          ) : (
+            <div>
+              <div className="MyPageMenu">
+                <div className="middleMenu">
+                  <NavLink to="/mypage" className="selectBtn" activeClassName="selectedBtn" exact>
+                    통계
+                  </NavLink>
+                  <NavLink to="/mypage/myinfo" className="selectBtn" activeClassName="selectedBtn">
+                    내 정보
+                  </NavLink>
                 </div>
-            ):(<div/>)}
-           {loginUser.type === "general" ? (
-          <div className="contentBox">
-            <GeneralFirstTab />
-            <Switch>
-              <Route path={`/mypage`} component={MyConsult} exact={true} />
-              <Route path={`/mypage/myinfo`} component={MyInfo} />
-            </Switch>
-            {rightTab === "freq" ? <MyFrequent /> : <GeneralThirdTab />}
-          </div>
-           ) : (
-               <div>
-                   <div className="MyPageMenu">
-                       <div className="middleMenu">
-                           <NavLink to="/mypage" className="selectBtn" activeClassName="selectedBtn" exact>
-                               통계
-                           </NavLink>
-                           <NavLink to="/mypage/myinfo" className="selectBtn" activeClassName="selectedBtn">
-                               내 정보
-                           </NavLink>
-                           {isPortfolioExist?(
-                               <NavLink to={`/portfolio/detail/` + loginUser.id} className="selectBtn" activeClassName="selectedBtn">
-                                   포트폴리오 보기
-                               </NavLink>
-                           ):(
-                               <NavLink to={`/portfolio/write`} className="selectBtn" activeClassName="selectedBtn">
-                                   포트폴리오 작성
-                               </NavLink>
-                           )}
-                       </div>
-                   </div>
-                   <div className="contentBox">
-                       <StylistFirstTab/>
-                       <Switch>
-                           <Route path={`/mypage`} component={StylistStats} exact={true}/>
-                           <Route path={`/mypage/myinfo`} component={StylistMyInfo}/>
-                           <Route path={`/mypage/credit`} component={CreditHistory}/>
-                       </Switch>
-                       {/*<StylistSecondTab/>*/}
-                       <GeneralThirdTab/>
-                   </div>
-               </div>
-        )}
+              </div>
+              <div className="contentBox">
+                <StylistFirstTab />
+                <Switch>
+                  <Route path={`/mypage`} component={StylistStats} exact={true} />
+                  <Route path={`/mypage/myinfo`} component={StylistMyInfo} />
+                  <Route path={`/mypage/credit`} component={CreditHistory} />
+                </Switch>
+                {/*<StylistSecondTab/>*/}
+                <GeneralThirdTab />
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- frontend/src/components/MyPage/StylistFirstTab.jsx
+++ frontend/src/components/MyPage/StylistFirstTab.jsx
@@ -1,52 +1,86 @@
-import React, {useEffect, useState} from "react"
-import './MyPageMain.scss'
-import axios from 'axios'
-import {Link} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import "./MyPageMain.scss";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Withdraw from "../Payment/Withdraw";
 
 const StylistFirstTab = () => {
-    const loginUser = JSON.parse(window.sessionStorage.getItem('user'))
-    const [user, setUser] = useState({})
-    const [consultList, setConsultList] = useState([])
-    const [receivedCount, setReceivedCount] = useState(0)
-    const [sentCount, setSentCount] = useState(0)
-    const [progressCount, setProgressCount] = useState(0)
-    const [doneConsultCount, setDoneConsultCount] = useState(0)
+  const loginUser = JSON.parse(window.sessionStorage.getItem("user"));
+  const [user, setUser] = useState({});
+  const [consultList, setConsultList] = useState([]);
+  const [receivedCount, setReceivedCount] = useState(0);
+  const [sentCount, setSentCount] = useState(0);
+  const [progressCount, setProgressCount] = useState(0);
+  const [doneConsultCount, setDoneConsultCount] = useState(0);
+  const [isPortfolioExist, setIsPortfolioExist] = useState(true);
 
-    useEffect(() => {
-        get_user()
-    }, [])
+  useEffect(() => {
+    get_user();
+    axios.get(`${process.env.REACT_APP_URL}/portfolio/check?stylist_id=` + loginUser.id).then((res) => {
+      if (res.data.isExist === "true") {
+        setIsPortfolioExist(true);
+      } else {
+        setIsPortfolioExist(false);
+      }
+    });
+  }, []);
 
-    const get_user = () => {
-        axios.get(`${process.env.REACT_APP_URL}/user/myinfo?user_id=` + loginUser.id)
-            .then(res => {
-                setUser(res.data.user)
-            })
-    }
+  const get_user = () => {
+    axios.get(`${process.env.REACT_APP_URL}/user/myinfo?user_id=` + loginUser.id).then((res) => {
+      setUser(res.data.user);
+    });
+  };
 
-    return (
-        <div className="left_tab">
-            <div className="center">
-                <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/cbdef037365169.573db7853cebb.jpg" alt={"Profile Image"} className="profile"/>
-            </div>
-            <div className="center topMargin">{user.name}</div>
-            <div className="center">스타일리스트</div>
-            <div className="center middleTopMargin">{user.credit} Point</div>
-            <div className="center">
-                <div className="smallSelectBtn">출금하기</div>
-                <Link to={"/mypage/credit"} className="smallSelectBtn">히스토리</Link>
-            </div>
-            <div className="center middleTopMargin">
-                <table>
-                    <tbody>
-                        <tr><td className="tdl">진행중 상담</td><td className="tdr">{progressCount}</td></tr>
-                        <tr><td className="tdl">받은 요청</td><td className="tdr">{receivedCount}</td></tr>
-                        <tr><td className="tdl">보낸 요청</td><td className="tdr">{sentCount}</td></tr>
-                        <tr><td className="tdl">완료된 상담</td><td className="tdr">{doneConsultCount}</td></tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="left_tab">
+      <div className="center">
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/cbdef037365169.573db7853cebb.jpg" alt={"Profile Image"} className="profile" />
+      </div>
+      <div className="center topMargin nickname">{user.name}</div>
+      <div className="center">스타일리스트</div>
+      <div className="middleTopMargin creditBox">
+        <div className="credit">{user.credit}</div>
+        <div>Point</div>
+      </div>
+      <div className="center">
+        <Withdraw />
+        <Link to={"/mypage/credit"} className="smallSelectBtn">
+          히스토리
+        </Link>
+      </div>
+      <div className="center middleTopMargin">
+        <table>
+          <tbody>
+            <tr>
+              <td className="tdl">진행중 상담</td>
+              <td className="tdr">{progressCount}</td>
+            </tr>
+            <tr>
+              <td className="tdl">받은 요청</td>
+              <td className="tdr">{receivedCount}</td>
+            </tr>
+            <tr>
+              <td className="tdl">보낸 요청</td>
+              <td className="tdr">{sentCount}</td>
+            </tr>
+            <tr>
+              <td className="tdl">완료된 상담</td>
+              <td className="tdr">{doneConsultCount}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      {isPortfolioExist ? (
+        <Link to={`/portfolio/detail/` + loginUser.id} className="portfolioBtn">
+          포트폴리오 보기
+        </Link>
+      ) : (
+        <Link to={`/portfolio/write`} className="portfolioBtn">
+          포트폴리오 작성
+        </Link>
+      )}
+    </div>
+  );
+};
 
-export default StylistFirstTab
+export default StylistFirstTab;
