81e6b3d7d5029979b9027f688225f65139562cca
silloun <47407845+silloun@users.noreply.github.com>
2020-06-08 12:09:19 +0900

    0608 | update statisticsAPI (not yet) | JH

---
+++ backend/controllers/statisticsConstroller.js
@@ -0,0 +1,11 @@
+import sequelize from 'sequelize'
+
+export const review_statistics = async (req,res) => {
+    try {
+        const {stylist_id} = req.query;
+        
+
+    } catch (error) {
+        
+    }
+}
